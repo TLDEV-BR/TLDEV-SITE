887935071acce81766a8758a46c8335fecfa1f8f
Tiago Xavier Braga <braga.tiagoxavier@gmail.com>
2022-01-11 16:31:26 -0300

    routes and front-end

--- app.py
+++ app.py
@@ -2,9 +2,17 @@ from flask import Flask, render_template, url_for
 
 app = Flask(__name__)
 
-@app.route('/', methods=['GET'])
+@app.route('/', methods = ['GET'])
 def index():
     return render_template('index.html')
+
+@app.route('/hire', methods = ['GET'])
+def hire():
+    return render_template('hire.html')
+
+@app.route('/about', methods = ['GET'] )
+def about():
+    return render_template('about.html')
 
 if __name__ == '__main__':
     app.run(debug = True)
